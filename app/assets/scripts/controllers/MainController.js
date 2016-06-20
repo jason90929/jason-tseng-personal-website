f7734b29016b8ff3d94d6659dd3611bc4a00ea5a
Jason Tseng <tsaistorm@gmail.com>
2016-06-20 14:49:43 +0800

    scroll lock add spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36

--- app/assets/scripts/controllers/MainController.js
+++ app/assets/scripts/controllers/MainController.js
@@ -110,7 +110,7 @@ module.exports = function($scope, $rootScope, $location) {
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var keys = {37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1};
 function preventDefault(e) {
     e = e || window.event;
     if (e.preventDefault)
